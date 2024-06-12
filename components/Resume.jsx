3a5c733baa35cb2eec027efaec818e8fd8837c4f
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-06-13 02:27:22 +0530

    added resume

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Resume = () => {
   return (
     <main className="flex flex-col sm:flex-row justify-between w-full rounded-[50px] shadow-lg bg-[#A2DCD5] px-10 max-w-2xl hover:scale-105 transition-all duration-500">
       <div className="flex pt-3">
-        <button className="p-3 sm:p-6 px-6 sm:px-9 border rounded-[50px] mx-auto my-auto bg-[#2A5F48] text-white text-md sm:text-2xl hover:translate-x-[-2px] hover:translate-y-[-2px] duration-500 hover:shadow-[3px_3px_0px_0px_rgba(42,95,72)]">
-          Résumé
-        </button>
+        <Link
+          href={
+            "https://www.dl.dropboxusercontent.com/scl/fi/s6u9l668q5chgcdhzo40d/Miuru-Abeysiriwardana.pdf?rlkey=fmt1jf8e87guux1xn39d5uoei&st=ax8d6e1a&dl=1"
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-3 sm:p-6 px-6 sm:px-9 border rounded-[50px] mx-auto my-auto bg-[#2A5F48] text-white text-md sm:text-2xl hover:translate-x-[-2px] hover:translate-y-[-2px] duration-500 hover:shadow-[3px_3px_0px_0px_rgba(42,95,72)]"
+        >
+          <button>Resumé</button>
+        </Link>
       </div>
       <div>
         <Image
